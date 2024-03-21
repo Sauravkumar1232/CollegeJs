cd4b68e6d2a477ab3399e83aeef5367fd4a5aef3
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-03-21 11:12:33 +0530

    async timeout

--- CallBackAsync.js
+++ CallBackAsync.js
@@ -1,20 +1,35 @@
 console.log('welcome')
 let user=getUser(1)
 console.log(user,'user')
+
 let repos=getRepos(user.name)
 console.log(repos)
+
 let commits=getCommit(repos[0])
 console.log(commits ,'commit')
+
 console.log('end..')
+
 function getUser(id){
+setTimeout(() => {
     return {id: id,name:'Nausad'}
+    
+}, 5000);
 
 }
+
 function getRepos(name){
-    return ['p1','p2','p3']
+    setTimeout(() => {
+        return ['p1','p2','p3']
+        
+    }, 3000);
 
 }
+
 function getCommit(repo){
-    return ['c1','c2','c3']
+    setTimeout(() => {
+        
+        return ['c1','c2','c3']
+    }, 2000);
 
 }
