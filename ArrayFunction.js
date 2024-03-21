9b0ea59f38e42f3ff7bd2a9b60cd5a94313ad152
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-03-21 10:51:07 +0530

    call back async

---
+++ ArrayFunction.js
@@ -0,0 +1,8 @@
+var marks=[50,40,45,37,20];  
+  
+function check(value)  
+{  
+  return value>30;    
+}  
+  
+marks.filter(check);  
--- functionAsync.js
+++ functionAsync.js
@@ -1,24 +1,59 @@
 
-console.log("Start")
+// console.log("Start")
 
-let students = [
-  { name: "Ram", age: 20 },
-  { name: "Mohan", age: 22 },
-];
+// let students = [
+//   { name: "Ram", age: 20 },
+//   { name: "Mohan", age: 22 },
+// ];
 
-function addStudent(){
+// function addStudent(callBack){
+//     setTimeout(function () {
+//         students.push({name:'Rohan',age:22})
+//         callBack();
+//     }, 5000);
+// }
+// function display(){
+//     setTimeout(function () {
+//          students.forEach(element => {
+//             console.log(element.name)
+//          })
+//     }, 3000);
+// }
+
+// addStudent(display)
+// // display()
+// console.log("End")
+
+
+let employess=[
+    {
+        name:"black",
+        address:"Mohan Nagar",
+        sal:20000
+    },
+    {
+        name:"pink",
+        address:"Vijay Nagar",
+        sal:20000
+    },{
+        name:"Red",
+        address:"Balram Nagar",
+        sal:20000
+    },
+
+]
+function   addEmployee(callBack){
+   setTimeout(function () {
+       employess.push({name:'Rohan',address:"Loni",sal:21000})
+       callBack();
+   }, 5000);
+   }
+
+   function display(){
     setTimeout(function () {
-        students.push({name:'Rohan',age:22})
-    }, 5000);
-}
-function display(){
-    setTimeout(function () {
-         students.forEach(element => {
+         employess.forEach(element => {
             console.log(element.name)
-         });
+         })
     }, 3000);
 }
-
-addStudent()
-display()
-console.log("End")
+addEmployee(display);
