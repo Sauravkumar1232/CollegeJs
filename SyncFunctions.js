9d8096bbe391c307e1f34eb1eaa86fe29bd09ae7
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-03-23 13:17:55 +0530

    reduce function

---
+++ SyncFunctions.js
@@ -0,0 +1,26 @@
+console.log('welcome')
+let user=getUser(1)
+console.log(user,'user')
+
+let repos=getRepos(user.name)
+console.log(repos)
+
+let commits=getCommit(repos[0])
+console.log(commits ,'commit')
+
+console.log('end..')
+
+function getUser(id){
+    return {id: id,name:'Nausad'}
+
+}
+
+function getRepos(name){
+    return ['p1','p2','p3']
+
+}
+
+function getCommit(repo){
+    return ['c1','c2','c3']
+
+}
