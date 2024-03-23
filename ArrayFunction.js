548f5d12d60156ef3fdda91820361d2a46733635
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-03-23 13:17:12 +0530

    reduce function

--- ArrayFunction.js
+++ ArrayFunction.js
@@ -1,3 +1,5 @@
+const { IdNumberIcon } = require("evergreen-ui");
+
 var marks=[50,40,45,37,20];  
   
 function check(value)  
@@ -6,3 +8,29 @@ function check(value)
 }  
   
 marks.filter(check);  
+
+let a=[10,20,30,79,90]
+
+function double(value){
+    return value*2
+}
+console.log( a.map(double))
+// console.log(a.findIndex(30))
+
+
+const array1 = [10,20,30,40,50];
+
+const isNumber = (element) => element == 30;
+
+console.log(array1.findIndex(isNumber));
+console.log('Sum',array1.reduce(getSum,0))
+array1.reduce(getSum,0)
+
+function getSum(total,num){
+    return total+IdNumberIcon
+}
+
+var sum = array1.reduce(function(element,currentValue){
+    return element-currentValue
+},0)
+console.log('some ',sum)
