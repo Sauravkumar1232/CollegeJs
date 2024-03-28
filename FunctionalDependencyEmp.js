56cc986afb74264556e1b87d02bf9cb88b870b67
SauravKumar1276 <kumarsaurav11494@gmail.com>
2024-03-28 11:20:53 +0530

    promise solve

--- FunctionalDependencyEmp.js
+++ FunctionalDependencyEmp.js
@@ -1,29 +1,34 @@
 // let user = { id: 1, name: "saurav" };
 console.log("start");
-getUser(1)
-  .then((user) => {
-    console.log(user, "user");
-    return getRepos(user.name);
-})
-// .catch((error) => {
-//   console.log("error", error);  // give error user undefined because of repos does not any value
+// getUser(1)
+//   .then((user) => {
+//     console.log(user, "user");
+//     return getRepos(user.name);
 // })
-  .then((repos) => {
-    console.log(repos, "user");
-    return getCommit(repos[0]);
-  })
-  .then((commits) => {
-    console.log(commits, "commits");
-  }).catch((error) => {
-       console.log("error", error);
-     })
+// // .catch((error) => {
+// //   console.log("error", error);  // give error user undefined because of repos does not any value
+// // })
+//   .then((repos) => {
+//     console.log(repos, "user");
+//     return getCommit(repos[0]);
+//   })
+//   .then((commits) => {
+//     console.log(commits, "commits");
+//   }).catch((error) => {
+//        console.log("error", error);
+//      })
+getUser(11)
+.then(user=> getRepos(user.name))
+.then(repos=>getCommit(repos[0]))
+.then(commit=>console.log(commit,'commit'))
+.catch((error)=>{console.log('error'),error})
 
 console.log("end..");
 function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("fetch the user data from db");
-      if (id === 11) {
+      if (id === 1) {
         resolve({ id: id, name: "saurav" });
       } else {
         reject("data not found");
